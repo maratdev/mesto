185ac1ec44ac453cc757d2a58d2594b1e5dc4158
maratdev <voredev@gmail.com>
2023-02-11 12:36:29 +0300

    feat(adaptive): реализация 1280px

--- js/index.js
+++ js/index.js
@@ -7,11 +7,10 @@ let itemCollection = document.querySelectorAll('.elements__items .elements__like
 
 
 // --------------------------------------------------------------------------------------------POPUP
-let popupProfileEdit = document.querySelector('.popup'),
-    profileEdit = document.querySelector('.profile__edit-btn'),
-    popupProfileEditClose = popupProfileEdit.querySelector('.popup__close'),
+let profileEdit = document.querySelector('.profile__edit-btn'),
     profileName = document.querySelector('.profile__name'),
     profileSubtitle = document.querySelector('.profile__subtitle'),
+    popupProfileEdit = document.querySelector('.popup'),
     nameInput = popupProfileEdit.querySelector('input[name="user_name"]'),
     jobInput = popupProfileEdit.querySelector('input[name="user_job"]');
 
@@ -24,16 +23,16 @@ profileEdit.addEventListener('click', function () {
   document.body.style.overflow = "hidden";
 });
 
-// Закрывания POPUP при клике X
+// Закрывание POPUP при клике X
 document.addEventListener('click', function (e) {
-  // Закрывания POPUP при клике снаружи его
+  // Закрывание POPUP при клике снаружи его
   if(e.target.classList.contains('popup') || e.target.classList.contains('popup__close')){
     popupProfileEdit.classList.remove('popup_opened');
-    document.body.style.overflow = "auto";
+    document.body.removeAttribute("style");
   }
 });
 
-// Закрывания POPUP при нажатии ESC
+// Закрывание POPUP при нажатии ESC
 document.addEventListener('keydown', function(e) {
   if (e.code == "Escape"){
     popupProfileEdit.classList.remove('popup_opened');
@@ -42,6 +41,7 @@ document.addEventListener('keydown', function(e) {
 
 // --------------------------------------------------------------------------------------------POPUP -> form
 
+//Обработчик «отправки» формы
 function handleFormSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
