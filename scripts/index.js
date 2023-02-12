13a81a8daaea60a36de8796de9357e5e5a59b21e
maratdev <voredev@gmail.com>
2023-02-12 14:54:50 +0300

    bugfix(popup js)

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,26 @@
-// Лайки в секции elements
-let itemCollection = document.querySelectorAll('.items .elements__like');
-  itemCollection.forEach(item => item.addEventListener('click', function(){
-    item.classList.toggle('elements__like_active');
-    })
-);
-
-
 // --------------------------------------------------------------------------------------------POPUP
 let profileEdit = document.querySelector('.profile__edit-btn'),
     profileName = document.querySelector('.profile__name'),
     profileSubtitle = document.querySelector('.profile__subtitle'),
+
     popupProfileEdit = document.querySelector('.popup'),
-    nameInput = popupProfileEdit.querySelector('input[name="user_name"]'),
-    jobInput = popupProfileEdit.querySelector('input[name="user_job"]');
+    closeInput = popupProfileEdit.querySelector('.popup__close'),
+    nameInput = popupProfileEdit.querySelector('.popup__input-text'),
+    jobInput = popupProfileEdit.querySelector('.popup__input-job'),
+    form = popupProfileEdit.querySelector('.popup__form');
 
 
 // Открытие POPUP при клике popup_opened
-profileEdit.addEventListener('click', function () {
+function openToClick() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileSubtitle.textContent;
   popupProfileEdit.classList.add('popup_opened');
-  document.body.style.overflow = "hidden";
-});
+}
 
 // Закрывание POPUP при клике X
-document.addEventListener('click', function (e) {
-  // Закрывание POPUP при клике снаружи его
-  if(e.target.classList.contains('popup') || e.target.classList.contains('popup__close')){
-    popupProfileEdit.classList.remove('popup_opened');
-    document.body.removeAttribute("style");
-  }
-});
-
-// Закрывание POPUP при нажатии ESC
-document.addEventListener('keydown', function(e) {
-  if (e.code == "Escape"){
-    popupProfileEdit.classList.remove('popup_opened');
-  }
-});
+function closeToClick() {
+  popupProfileEdit.classList.remove('popup_opened');
+}
 
 // --------------------------------------------------------------------------------------------POPUP -> form
 
@@ -46,11 +29,12 @@ function handleFormSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
-  popupProfileEdit.classList.remove('popup_opened');
-  document.body.removeAttribute("style");
+  closeToClick();
 }
 
-popupProfileEdit.addEventListener('submit', handleFormSubmit);
+form.addEventListener('submit', handleFormSubmit);
+profileEdit.addEventListener('click', openToClick)
+closeInput.addEventListener('click', closeToClick)
 
 
 
