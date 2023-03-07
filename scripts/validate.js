0ae0f38de8d0d702dcdd8d3154d7668bfd90fc66
maratdev <voredev@gmail.com>
2023-03-07 16:26:01 +0300

    feat: реструктуризация файловой структуры + каталогов

---
+++ scripts/validate.js
@@ -0,0 +1,8 @@
+const object = {
+  formSelector: '.form',
+  inputSelector: '.form__input',
+  submitSelector: '.form__button-submit',
+  disabledButtonClass: 'form__button-submit_disabled',
+  errorClass: 'form__span-error',
+  inputErrorClass: 'form__input-error',
+};
