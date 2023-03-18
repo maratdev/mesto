1293c8472d4e9acfecc03615762942b689609606
maratdev <voredev@gmail.com>
2023-03-18 23:37:32 +0300

    feat(ПР7) реализация class Card

---
+++ scripts/Card.js
@@ -0,0 +1,37 @@
+export default class Card {
+  constructor(name, link, templateSelector, initImageModalOpen) {
+    this._templateSelector = templateSelector;
+    this._name = name;
+    this._link = link;
+    this._initImageModalOpen = initImageModalOpen;
+  }
+
+  _getTemplate() {
+    const newElement = document.querySelector(this._templateSelector).content.querySelector('.elements__items').cloneNode(true);
+    return newElement;
+  }
+
+  generateCard() {
+    // Запишем разметку в приватное поле _element.
+    // Так у других элементов появится доступ к ней.
+    this._element = this._getTemplate();
+
+    // Добавим данные
+    this._cardImage = this._element.querySelector(".card__item");
+    this._element.querySelector('.card__item').src = this._link;
+    this._element.querySelector('.card__item').alt = this._name;
+    this._element.querySelector('.card__title').textContent = this._name;
+
+    this._setEventListeners();
+
+    // Вернём элемент наружу
+    return this._element;
+  }
+
+  _setEventListeners() {
+    this._cardImage.addEventListener("click", () => {
+       this._initImageModalOpen(this._element);
+    });
+  };
+
+}
